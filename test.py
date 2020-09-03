7ac92ecc1649f45009c233b0d715b1f73ff64644
62fe50 <merrickslane@gmail.com>
2020-09-02 20:37:25 -0700

    test

--- test.py
+++
@@ -1,9 +0,0 @@
-import discord
-
-client = discord.ext.commands.Bot(command_prefix = "$")
-
-@client.command(brief="description")
-async def hello(ctx):
-    #stuff you want it to do
-
-client.run("d28m3hfuagyufag67tgweq768rf")
